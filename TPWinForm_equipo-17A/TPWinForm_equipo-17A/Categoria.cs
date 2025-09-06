371bfbc2b09f2612d38367201517dbc5aba109b2
natudellasavia <80315756+natudellasavia@users.noreply.github.com>
2025-09-06 21:35:57 +0100

    creacion clase Marca y clase Categoria

---
+++ TPWinForm_equipo-17A/TPWinForm_equipo-17A/Categoria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_17A
+{
+    internal class Categoria
+    {
+        // ATRIBUTOS
+        public int id { get; set; } /// Que sea autoincrementable tomando de referencia el último que encuentre?
+        public string descripcion { get; set; }
+
+        // CONSTRUCTOR
+        public Categoria(int id, string descripcion)
+        {
+            this.id = id;
+            this.descripcion = descripcion;
+        }
+    }
+}
---
+++ TPWinForm_equipo-17A/TPWinForm_equipo-17A/Marca.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_17A
+{
+    internal class Marca
+    {
+        // ATRIBUTOS
+        public int id { get; set; } /// Que sea autoincrementable tomando de referencia el último que encuentre?
+        public string descripcion { get; set; }
+        
+        // CONSTRUCTOR
+        public Marca(int id, string descripcion)
+        {
+            this.id = id;
+            this.descripcion = descripcion;
+        }
+    }
+}
