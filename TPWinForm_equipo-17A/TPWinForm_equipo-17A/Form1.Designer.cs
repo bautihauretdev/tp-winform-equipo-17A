475d2e663322f1692cbf514f2808884f1cd717bc
natudellasavia <80315756+natudellasavia@users.noreply.github.com>
2025-09-08 20:37:36 +0100

    Creacion menu principal y botones

--- TPWinForm_equipo-17A/TPWinForm_equipo-17A/Form1.Designer.cs
+++ TPWinForm_equipo-17A/TPWinForm_equipo-17A/Form1.Designer.cs
@@ -28,21 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnArticulos = new System.Windows.Forms.Button();
+            this.lblMenuPrincipal = new System.Windows.Forms.Label();
+            this.btnMarcas = new System.Windows.Forms.Button();
+            this.btnCategorias = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnArticulos
+            // 
+            this.btnArticulos.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnArticulos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnArticulos.Location = new System.Drawing.Point(220, 167);
+            this.btnArticulos.Name = "btnArticulos";
+            this.btnArticulos.Size = new System.Drawing.Size(169, 46);
+            this.btnArticulos.TabIndex = 0;
+            this.btnArticulos.Text = "&Artículos";
+            this.btnArticulos.UseVisualStyleBackColor = true;
+            // 
+            // lblMenuPrincipal
+            // 
+            this.lblMenuPrincipal.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblMenuPrincipal.AutoSize = true;
+            this.lblMenuPrincipal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMenuPrincipal.Location = new System.Drawing.Point(213, 121);
+            this.lblMenuPrincipal.Name = "lblMenuPrincipal";
+            this.lblMenuPrincipal.Size = new System.Drawing.Size(181, 29);
+            this.lblMenuPrincipal.TabIndex = 3;
+            this.lblMenuPrincipal.Text = "Menú Principal";
+            this.lblMenuPrincipal.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnMarcas
+            // 
+            this.btnMarcas.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnMarcas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMarcas.Location = new System.Drawing.Point(220, 231);
+            this.btnMarcas.Name = "btnMarcas";
+            this.btnMarcas.Size = new System.Drawing.Size(169, 48);
+            this.btnMarcas.TabIndex = 4;
+            this.btnMarcas.Text = "&Marcas";
+            this.btnMarcas.UseVisualStyleBackColor = true;
+            // 
+            // btnCategorias
+            // 
+            this.btnCategorias.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnCategorias.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCategorias.Location = new System.Drawing.Point(220, 299);
+            this.btnCategorias.Name = "btnCategorias";
+            this.btnCategorias.Size = new System.Drawing.Size(169, 48);
+            this.btnCategorias.TabIndex = 5;
+            this.btnCategorias.Text = "&Categorias";
+            this.btnCategorias.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ClientSize = new System.Drawing.Size(609, 570);
+            this.Controls.Add(this.btnCategorias);
+            this.Controls.Add(this.btnMarcas);
+            this.Controls.Add(this.lblMenuPrincipal);
+            this.Controls.Add(this.btnArticulos);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(627, 617);
+            this.MinimumSize = new System.Drawing.Size(627, 617);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnArticulos;
+        private System.Windows.Forms.Label lblMenuPrincipal;
+        private System.Windows.Forms.Button btnMarcas;
+        private System.Windows.Forms.Button btnCategorias;
     }
 }
 
