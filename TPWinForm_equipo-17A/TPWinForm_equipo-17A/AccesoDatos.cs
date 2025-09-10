54a20b5cc627ba92793909b7a50d4c706ea32879
BautistaHauret <bautihauret@gmail.com>
2025-09-10 16:04:30 -0300

    agrego clase AccesoDatos para facil uso con la bdd

---
+++ TPWinForm_equipo-17A/TPWinForm_equipo-17A/AccesoDatos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.ComponentModel;
+
+namespace TPWinForm_equipo_17A
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public AccesoDatos()
+        {
+            // Cambia el nombre del servidor y la base según tu configuración
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security= true");
+            comando = new SqlCommand();
+        }
+
+        public SqlDataReader ejecutarLectura(string consulta)
+        {
+            comando.CommandType = CommandType.Text;
+            comando.CommandText = consulta;
+            comando.Connection = conexion;
+
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+                return lector;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            if (conexion.State == ConnectionState.Open)
+                conexion.Close();
+        }
+    }
+}
